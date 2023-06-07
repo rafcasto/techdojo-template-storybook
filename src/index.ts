22dada987ca94bee0a10251b82094e1b1e778a02
Rafael Castillo <rafcasto@gmail.com>
2023-06-08 00:06:12 +1200

    adding package

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import {AlertComponent} from './stories/components/Alert/AlertComponent'
+import {CalculateButton} from './stories/components/Button/CalculateButton'
+import {InputText} from './stories/components/InputText/InputText'
+import {Summary} from './stories/components/Summary/Summary'
+export {
+  AlertComponent,
+  CalculateButton,
+  InputText,
+  Summary
+}
